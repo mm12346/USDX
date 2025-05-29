29e62e7fc17de5a6d8931fbec9641ff08aabe7b2
mm12346 <xoctell338@gmail.com>
2025-05-29 19:20:24 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // กำหนดชื่อแคชและเวอร์ชัน
-const CACHE_NAME = 'usdx-cache-v1.0.1'; // เปลี่ยนเวอร์ชันเมื่อมีการอัปเดตไฟล์
+const CACHE_NAME = 'usdx-cache-v1.0.2'; // เปลี่ยนเวอร์ชันเมื่อมีการอัปเดตไฟล์
 const urlsToCache = [
   '/',
   '/index.html',
